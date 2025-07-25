20c878eeb92d237ab07fcb845034ffa02074a3cb
Siam300 <89306483+Siam300@users.noreply.github.com>
2025-07-25 20:29:14 +0600

    UIKit Custom Tab bar.

--- UIKitTabBar/ViewController.swift
+++ UIKitTabBar/ViewController.swift
@@ -9,11 +9,97 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let loginButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        button.backgroundColor = .white
+        button.setTitleColor(.black, for: .normal)
+        button.setTitle("Login", for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemBlue
+        view.addSubview(loginButton)
+        
+        loginButton.addTarget(self, action: #selector(didTapLoginButton), for: .touchUpInside)
     }
-
-
+    
+    @objc func didTapLoginButton() {
+        let tabBarVC = UITabBarController()
+        
+        let vc1 = UINavigationController(rootViewController: FirstViewController())
+        let vc2 = UINavigationController(rootViewController: SecondViewController())
+        let vc3 = UINavigationController(rootViewController: ThirdViewController())
+        let vc4 = UINavigationController(rootViewController: FourthViewController())
+        let vc5 = UINavigationController(rootViewController: FifthViewController())
+        
+        vc1.title = "Home"
+        vc2.title = "Contact"
+        vc3.title = "Help"
+        vc4.title = "About"
+        vc5.title = "Settings"
+        
+        tabBarVC.tabBar.backgroundColor = .white
+        tabBarVC.setViewControllers([vc1, vc2, vc3, vc4, vc5], animated: false)
+        
+        guard let items = tabBarVC.tabBar.items else { return }
+        
+        let images = ["house", "person.fill", "questionmark.circle", "info.circle", "gear"]
+        
+        for x in 0..<items.count {
+            items[x].badgeValue = "1"
+            items[x].image = UIImage(systemName: images[x])
+        }
+        
+        tabBarVC.modalPresentationStyle = .fullScreen
+        present(tabBarVC, animated: true)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        loginButton.center = view.center
+    }
 }
 
+class FirstViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+        title = "Home"
+    }
+}
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        title = "Contact"
+    }
+}
+
+class ThirdViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        title = "Help"
+    }
+}
+
+class FourthViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        title = "About"
+    }
+}
+
+class FifthViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+        title = "Settings"
+    }
+}
